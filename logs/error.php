0f5d1d7a9e540fa55ce0026e8665e23f74a09c1b
Ale Sandro <alexrah@gmail.com>
2014-02-25 19:46:38 +0100

    initial commit

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-02-24 09:33:45 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-02-24T09:33:45+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-02-25T16:24:20+00:00	INFO	cookiefailure	Empty password not allowed
