64de2d6caa3e2174e7c7f094477c5f6396cb7443
Ale Sandro <alexrah@gmail.com>
2015-12-20 18:47:13 +0100

    db: aruba drop

--- logs/error.php
+++ logs/error.php
@@ -20,3 +20,10 @@
 2014-03-03T22:55:28+00:00	INFO	cookiefailure	Username e Password non coincidono o non hai ancora un account
 2014-03-03T23:00:07+00:00	INFO	cookiefailure	Password vuota non consentita
 2014-04-09T12:54:02+00:00	INFO	cookiefailure	Username e Password non coincidono o non hai ancora un account
+2014-06-03T16:08:24+00:00	INFO	joomlafailure	Nome utente e password non corretti o non hai ancora un account.
+2014-06-29T08:53:02+00:00	INFO	joomlafailure	Nome utente e password non corretti o non hai ancora un account.
+2014-07-02T17:01:12+00:00	INFO	joomlafailure	Nome utente e password non corretti o non hai ancora un account.
+2014-07-02T18:44:26+00:00	INFO	joomlafailure	Nome utente e password non corretti o non hai ancora un account.
+2014-07-14T00:06:47+00:00	INFO	joomlafailure	Nome utente e password non corretti o non hai ancora un account.
+2014-07-29T23:47:43+00:00	INFO	joomlafailure	Nome utente e password non corretti o non hai ancora un account.
+2014-07-29T23:53:19+00:00	INFO	joomlafailure	Nome utente e password non corretti o non hai ancora un account.
